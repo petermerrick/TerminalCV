06e9bf78029f803b3ce5ea915247b54a0e67182e
peter merrick <peter.merrick@adam.com.au>
2019-08-17 19:13:12 +0930

    updated

--- index.js
+++ index.js
@@ -103,6 +103,14 @@ Typer.init();
 var numberOfRowsTyped = 0;
 var timer = setInterval("t();", 30);
 function t() {
+	if(numberOfRowsTyped>68)
+	{
+		Typer.speed=10;
+	}
+	if(numberOfRowsTyped>173)
+	{
+		Typer.speed=3;
+	}
 	Typer.addText({"keyCode": 123748});
 	numberOfRowsTyped += 1;
 	console.log("test this out");
